0c7fc53848896e3cd6500c359f79896d205c6e44
Loek van Gent <loek@goodup.com>
2019-08-03 12:45:13 +0200

    Django update again and limi user admin fields

--- apps/users/admin.py
+++ apps/users/admin.py
@@ -4,6 +4,8 @@ from apps.users.models import User
 
 class UserAdmin(admin.ModelAdmin):
     list_display = ('username', 'email', 'first_name', 'last_name')
-
+    readonly_fields = ('last_login', 'date_joined')
+    fields = ('username', 'email', 'first_name', 'last_name',
+              'is_active', 'is_superuser', 'is_staff') + readonly_fields
 
 admin.site.register(User, UserAdmin)
